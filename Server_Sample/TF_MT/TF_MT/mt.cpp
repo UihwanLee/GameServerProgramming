c9c85a4c8bdf05280127eb9eae99b7c60447d57c
UihwanLee <luh5063014@naver.com>
2024-04-09 15:44:19 +0900

    [6주차] 프로그램 성능 측정 코드 구현

--- Server_Sample/TF_MT/TF_MT/mt.cpp
+++ Server_Sample/TF_MT/TF_MT/mt.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <chrono>
+
+using namespace std::chrono;
+
+int sum = 0;
 
 void thread_worker(int th_id)
 {
-	std::cout << "Hello World From " 
-		<< th_id << std::endl;
+	
 }
 
 int main()
 {
-	std::vector<std::thread> my_threads;
+	int sum = 0;
+
+	auto start_t = high_resolution_clock::now();
+	for (int i = 0; i < 50000000; ++i)
+		sum = sum + 2;
+	auto end_t = high_resolution_clock::now();
+
+	auto exec_t = end_t - start_t;
+	auto exec_ms = duration_cast<milliseconds>(exec_t);
+
+	std::cout << "Sum = " << sum << "  duration = ";
+		std::cout << duration_cast<milliseconds>(exec_t).count() << std::endl;
+
+	/*std::vector<std::thread> my_threads;
 	for (int i = 0; i < 10; ++i) {
 		my_threads.emplace_back(thread_worker, i);
 	}
 	for (auto& th : my_threads) {
 		th.join();
-	}
+	}*/
 }
