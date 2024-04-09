6f15f1d37aa1fffe820d2924a1af5452ca2293a8
UihwanLee <luh5063014@naver.com>
2024-04-09 16:51:57 +0900

    [6주차] atomic을 이용한 멀티 쓰레드 프로그래밍 구현

--- Server_Sample/TF_MT/TF_MT/mt.cpp
+++ Server_Sample/TF_MT/TF_MT/mt.cpp
@@ -3,21 +3,35 @@
 #include <vector>
 #include <chrono>
 #include <mutex>
+#include <atomic>
 
 using namespace std::chrono;
 
 volatile int g_sum = 0;
 std::mutex sum_lock;
 
+std::atomic <int> a_sum;
+
 void thread_worker(const int num_th)
 {
-	volatile int local_sum = 0;
+	volatile int sum = 0;
 	for (auto i = 0; i < 50000000 / num_th; ++i)
 	{
-		local_sum = local_sum + 2;
+		sum = sum + 2;
 	}
 	sum_lock.lock();
-	g_sum = g_sum + local_sum;
+	g_sum = g_sum + sum;
+	sum_lock.unlock();
+}
+
+void a_thread_worker(const int num_th)
+{
+	for (auto i = 0; i < 50000000 / num_th; ++i)
+	{
+		a_sum = a_sum + 2;
+	}
+	sum_lock.lock();
+	g_sum = g_sum + a_sum;
 	sum_lock.unlock();
 }
 
@@ -42,7 +56,7 @@ int main()
 			g_sum = 0;
 			auto start_t = high_resolution_clock::now();
 			std::vector<std::thread> threads;
-			for (int i = 0; i < num_threads; ++i) 
+			for (int i = 0; i < num_threads; ++i)
 				threads.emplace_back(thread_worker, num_threads);
 
 			for (auto& th : threads)
@@ -54,7 +68,26 @@ int main()
 
 			std::cout << num_threads << " Thread Sum = " << g_sum << "  time = " << exec_ms << "ms." << std::endl;
 		}
+	}
 
+	{
+		for (int num_threads = 1; num_threads <= 16; num_threads *= 2) {
+			g_sum = 0;
+			auto start_t = high_resolution_clock::now();
+			std::vector<std::thread> threads;
+			for (int i = 0; i < num_threads; ++i)
+				threads.emplace_back(a_thread_worker, num_threads);
+
+			for (auto& th : threads)
+				th.join();
+
+			auto end_t = high_resolution_clock::now();
+			auto exec_t = end_t - start_t;
+			auto exec_ms = duration_cast<milliseconds>(exec_t).count();
+
+			std::cout << num_threads << " Thread Atomic Sum = " << a_sum << "  time = " << exec_ms << "ms." << std::endl;
+		}
+	}
 		/*std::vector<std::thread> my_threads;
 		for (int i = 0; i < 10; ++i) {
 			my_threads.emplace_back(thread_worker, i);
@@ -62,5 +95,4 @@ int main()
 		for (auto& th : my_threads) {
 			th.join();
 		}*/
-	}
 }
