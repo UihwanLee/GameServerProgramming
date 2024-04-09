0528a3938ea62cf3e574a8a7f5a27bd08d07f93c
UihwanLee <luh5063014@naver.com>
2024-04-09 16:22:36 +0900

    [6주차] lock unlock을 이용하여 Data Race 문제 해결

--- Server_Sample/TF_MT/TF_MT/mt.cpp
+++ Server_Sample/TF_MT/TF_MT/mt.cpp
@@ -2,15 +2,21 @@
 #include <thread>
 #include <vector>
 #include <chrono>
+#include <mutex>
 
 using namespace std::chrono;
 
 volatile int g_sum = 0;
+std::mutex sum_lock;
 
 void thread_worker(const int num_th)
 {
-	for (auto i = 0; i < 50000000 / num_th; ++i) 
+	sum_lock.lock();
+	for (auto i = 0; i < 50000000 / num_th; ++i)
+	{
 		g_sum = g_sum + 2;
+	}
+	sum_lock.unlock();
 }
 
 int main()
@@ -30,7 +36,8 @@ int main()
 	}
 
 	{
-		for (int num_threads = 2; num_threads <= 16; num_threads *= 2) {
+		for (int num_threads = 1; num_threads <= 16; num_threads *= 2) {
+			g_sum = 0;
 			auto start_t = high_resolution_clock::now();
 			std::vector<std::thread> threads;
 			for (int i = 0; i < num_threads; ++i) 
@@ -43,7 +50,7 @@ int main()
 			auto exec_t = end_t - start_t;
 			auto exec_ms = duration_cast<milliseconds>(exec_t).count();
 
-			std::cout << "2 Thread Sum = " << g_sum << "  time = " << exec_ms << "ms." << std::endl;
+			std::cout << num_threads << " Thread Sum = " << g_sum << "  time = " << exec_ms << "ms." << std::endl;
 		}
 
 		/*std::vector<std::thread> my_threads;
