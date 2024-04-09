846bc36e9c836e3b0f082e00e62e01f6a89168f9
UihwanLee <luh5063014@naver.com>
2024-04-09 16:04:25 +0900

    [6주차] 멀티 코어 개수만큼 쓰레드를 생성하여 구현

--- Server_Sample/TF_MT/TF_MT/mt.cpp
+++ Server_Sample/TF_MT/TF_MT/mt.cpp
@@ -5,33 +5,53 @@
 
 using namespace std::chrono;
 
-int sum = 0;
+volatile int g_sum = 0;
 
-void thread_worker(int th_id)
+void thread_worker(const int num_th)
 {
-	
+	for (auto i = 0; i < 50000000 / num_th; ++i) 
+		g_sum = g_sum + 2;
 }
 
 int main()
 {
-	volatile int sum = 0;
+	{
+		volatile int sum = 0;
 
-	auto start_t = high_resolution_clock::now();
-	for (int i = 0; i < 50000000; ++i)
-		sum = sum + 2;
-	auto end_t = high_resolution_clock::now();
+		auto start_t = high_resolution_clock::now();
 
-	auto exec_t = end_t - start_t;
-	auto exec_ms = duration_cast<milliseconds>(exec_t);
+		for (auto i = 0; i < 50000000; ++i) sum = sum + 2;
 
-	std::cout << "Sum = " << sum << "  duration = ";
-		std::cout << duration_cast<milliseconds>(exec_t).count() << std::endl;
+		auto end_t = high_resolution_clock::now();
+		auto exec_t = end_t - start_t;
+		auto exec_ms = duration_cast<milliseconds>(exec_t).count();
 
-	/*std::vector<std::thread> my_threads;
-	for (int i = 0; i < 10; ++i) {
-		my_threads.emplace_back(thread_worker, i);
+		std::cout << "sum = " << sum << "  time = " << exec_ms << "ms." << std::endl;
 	}
-	for (auto& th : my_threads) {
-		th.join();
-	}*/
+
+	{
+		for (int num_threads = 2; num_threads <= 16; num_threads *= 2) {
+			auto start_t = high_resolution_clock::now();
+			std::vector<std::thread> threads;
+			for (int i = 0; i < num_threads; ++i) 
+				threads.emplace_back(thread_worker, num_threads);
+
+			for (auto& th : threads)
+				th.join();
+
+			auto end_t = high_resolution_clock::now();
+			auto exec_t = end_t - start_t;
+			auto exec_ms = duration_cast<milliseconds>(exec_t).count();
+
+			std::cout << "2 Thread Sum = " << g_sum << "  time = " << exec_ms << "ms." << std::endl;
+		}
+
+		/*std::vector<std::thread> my_threads;
+		for (int i = 0; i < 10; ++i) {
+			my_threads.emplace_back(thread_worker, i);
+		}
+		for (auto& th : my_threads) {
+			th.join();
+		}*/
+	}
 }
