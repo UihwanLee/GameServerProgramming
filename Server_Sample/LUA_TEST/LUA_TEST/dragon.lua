6a9ec4fb0c06b07068f88823285f8334fbb6a109
UihwanLee <luh5063014@naver.com>
2024-05-14 16:14:33 +0900

    [11주차] LUA에서 C 함수를 정의하고 호출하여 add 함수 구현

--- Server_Sample/LUA_TEST/LUA_TEST/dragon.lua
+++ Server_Sample/LUA_TEST/LUA_TEST/dragon.lua
@@ -3,5 +3,12 @@ function plustwo (x)
  a = 2
  return x+a;
  end--here we see a simple function but it could be very complex,--even check for user input
+
+ function event_add(a, b)
+	local c
+	c = callC_add(a, b);
+	return c;
+ end
+
  pos_x = 6; -- comments in a config file can be handy
  pos_y = plustwo(pos_x);--now we can have functions in our config files.
--- Server_Sample/LUA_TEST/LUA_TEST/lua_main.cpp
+++ Server_Sample/LUA_TEST/LUA_TEST/lua_main.cpp
@@ -3,6 +3,16 @@
 
 #pragma comment (lib, "lua54.lib")
 
+int LUAapi_add(lua_State* L)
+{
+	int a = lua_tonumber(L, -1);
+	int b = lua_tonumber(L, -2);
+	lua_pop(L, 3);
+	int c = a + b;
+	lua_pushnumber(L, c);
+	return 1;
+}
+
 int main()
 {
 	const char* program = "print \"Hello from Lua\"";
@@ -22,9 +32,17 @@ int main()
 	int pos_y = lua_tonumber(L, -1);
 	lua_pop(L, 2);
 	std::cout << "POS_X = " << pos_x << ", POS_Y = " << pos_y << std::endl;*/
-	lua_getglobal(L, "plustwo");
+	lua_register(L, "callC_add", LUAapi_add);
+
+	lua_getglobal(L, "event_add");
 	lua_pushnumber(L, 10);
-	lua_pcall(L, 1, 1, 0);
+	lua_pushnumber(L, 20);
+	ret = lua_pcall(L, 2, 1, 0);
+	if (0 != ret) {
+		std::cout << "Error: " << lua_tostring(L, -1);
+		lua_pop(L, 1);
+		return 0;
+	}
 	int res = lua_tonumber(L, -1);
 	lua_pop(L, 1);
 	std::cout << "Result = " << res << std::endl;
