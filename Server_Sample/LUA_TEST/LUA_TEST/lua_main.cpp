0355031353a6007a3ef7432c2b1d79ef6f6e5e6b
UihwanLee <luh5063014@naver.com>
2024-05-14 15:56:38 +0900

    [11주차] lua 함수 호출하여 값 얻기

--- Server_Sample/LUA_TEST/LUA_TEST/lua_main.cpp
+++ Server_Sample/LUA_TEST/LUA_TEST/lua_main.cpp
@@ -16,11 +16,18 @@ int main()
 		lua_pop(L, 1);
 	}
 
-	lua_getglobal(L, "pos_x");
+	/*lua_getglobal(L, "pos_x");
 	lua_getglobal(L, "pos_y");
 	int pos_x = lua_tonumber(L, -2);
 	int pos_y = lua_tonumber(L, -1);
 	lua_pop(L, 2);
-	std::cout << "POS_X = " << pos_x << ", POS_Y = " << pos_y << std::endl;
+	std::cout << "POS_X = " << pos_x << ", POS_Y = " << pos_y << std::endl;*/
+	lua_getglobal(L, "plustwo");
+	lua_pushnumber(L, 10);
+	lua_pcall(L, 1, 1, 0);
+	int res = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+	std::cout << "Result = " << res << std::endl;
+	
 	lua_close(L);
 }
